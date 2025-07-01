d3a9eb849ea158077d4c479a286ffbf6bb6aaf6b
Joakim Animate <joakimbwire23@gmail.com>
2025-07-01 16:31:38 +0300

    Update environment variables and requirements; refactor settings and URL configurations

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -14,24 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.shortcuts import redirect
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="alx_travel_app API",
-        default_version="v1",
-        description="API documentation for alx_travel_app",
+        title='ALX Travel API',
+        default_version='v1',
+        description='API documentation for alx_travel_app',
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny]
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path('admin/', admin.site.urls),
+    re_path(r'^$', lambda request: redirect('swagger/', permanent=False)),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
